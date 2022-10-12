29728528b067c45a50ccb0fd408454b326a832cd
Adrian Maźnio <adrian.maznio@makingwaves.com>
2022-10-12 19:42:57 +0200

    hamburger

---
+++ scripts.js
@@ -0,0 +1,13 @@
+function toggleMenu() {
+  const navLinks = document.getElementById("mobile-menu");
+  if (navLinks) {
+    if (navLinks.classList.contains("d-flex")) {
+      navLinks.classList.remove("d-flex");
+      navLinks.classList.add("d-none");
+    }
+    else {
+      navLinks.classList.add("d-flex");
+      navLinks.classList.remove("d-none");
+    }
+  }
+}
