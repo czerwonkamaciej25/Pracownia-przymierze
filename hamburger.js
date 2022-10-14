61affb4eb54e331e76c93a7f44814fa93cf3e2e6
Maciej <czerwonkamaciej25@gmail.com>
2022-10-14 18:35:07 +0200

    mobile4

--- hamburger.js
+++
@@ -1,6 +0,0 @@
-$(window).load(function() {
-    $(".btn-nav").on("click tap", function() {
-      $(".nav-content").toggleClass("showNav hideNav").removeClass("hidden");
-      $(this).toggleClass("animated");
-    });
-  });
