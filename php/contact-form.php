28231cb8592af4e627fe202933d7841e3952136c
Maciej Czerwonka <czerwonkamaciej25@gmail.com>
2022-11-21 23:20:06 +0100

    v1.10

--- php/contact-form.php
+++ php/contact-form.php
@@ -6,8 +6,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   $email = $_POST['email'];     // Sender's email address
   $phone  = $_POST['phone'];     // Sender's email address
   $message = $_POST['message'];    // Sender's message
-  $from = 'PRACOWNIA PRZYMIERZE - zgłoszenie';
-  $to = 'przemek.traczynski@gmail.com';     // Recipient's email address
+  $from = 'PRACOWNIA PRZYMIERZE - formularz kontaktowy ';
+  $to = 'czerwonkamaciej25@gmail.com, przemek.traczynski@gmail.com';     // Recipient's email address
   $subject = 'PRACOWNIA PRZYMIERZE';
 
   $body ="From: $name \n E-Mail: $email \n Phone : $phone \n Message : $message"  ;
@@ -16,17 +16,17 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 	$errmsg = '';
   // Check if name has been entered
   if (!$_POST['name']) {
-   $errmsg = 'Please enter your name';
+   $errmsg = 'Pole wymagane';
   }
 
   // Check if email has been entered and is valid
   if (!$_POST['email'] || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
-   $errmsg = 'Please enter a valid email address';
+   $errmsg = 'Nieprawidłowy format adresu e-mail';
   }
 
   //Check if message has been entered
   if (!$_POST['message']) {
-   $errmsg = 'Please enter your message';
+   $errmsg = 'Pole wymagane';
   }
 
 	$result = '';
